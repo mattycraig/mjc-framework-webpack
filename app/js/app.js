8c4fe09523e7096480914d88bd944dfe92fda014
Matthew Craig <matt.craig@sandboxww.com>
2017-02-14 17:17:45 -0500

    Dep updates

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,8 @@
 //--------------------------------------------|
 // Load all of our js files
 
-require('app/js/etc/helpers');
 require('app/js/etc/vendor');
+require('app/js/etc/helpers');
 require('app/js/jquery/base');
 // require('app/js/angular/base');
 // require('app/js/react/base');
